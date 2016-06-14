cab37cdbb57f3fa3b944efe4cce4f88a73d8a46d
Charles Ewel and Kevin VanEvery <kevin@>
2016-06-14 11:53:02 -0700

    add javascript for withdrawing and depositing from account

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,40 @@
 /*This file is for your custom js.  All yours*/
+// business rules
+
+function Account (name, balance) {
+  this.name = name;
+  this.balance = balance;
+}
+
+Account.prototype.transfer = function(integer, deposit) {
+    this.balance += deposit * integer;
+}
+
+
+
+
 
 // Calls input from form-input.html
 
 $(document).ready(function(){
 
-  $("#input").submit(function(event){
+  var accountRegistry = [];
+
+  $("#account-register").submit(function(event){
     event.preventDefault();
-    var input = ($("#blank").val());
-
-    var output = "";
-
-    $('#output').text(output);
-
+    var accountholderName = ($("#name-accountholder").val());
+    var balance = parseInt($("#deposit-initial").val());
+    var newAccount = new Account(accountholderName, balance)
+    accountRegistry.push(newAccount);
+    $('#balance-current').text(newAccount.balance);
   });
+
+  $("#deposit-withdrawal").submit(function (event) {
+    event.preventDefault();
+    currentAccount=accountRegistry[0];
+    var amount = parseInt($("input#withdraw-deposit-amount").val());
+    var deposit = parseInt($("input:radio[name=withdraw-deposit]:checked").val());
+    currentAccount.transfer(amount, deposit);
+    $('#balance-current').text(currentAccount.balance);
+  })
 });
